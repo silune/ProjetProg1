90ff2d0109a8445f7b33e6634c27875898442a51
Simon C <corbard.simon@gmail.com>
2022-10-24 02:55:30 +0200

    Gestion des operateurs sur les entiers. Ajout des .mli

--- aritha/compiler.ml
+++ aritha/compiler.ml
@@ -5,13 +5,17 @@ open Parser
 
 let push_int r = pushq (reg r);;
 let pop_int r = popq r;;
-let get_two = (pop_int rdi) ++ (pop_int rsi);;
 
 let rec code_of tree =
+        let code t1 t2 = (code_of t1) ++ (code_of t2) in
         match tree with
         | INT x -> movq (imm (int_of_string x)) (reg rdi) ++ (push_int rdi)
-        | ADDI (t1, t2) -> (code_of t1) ++ (code_of t2) ++ (get_two) ++ (addq (reg rdi) (reg rsi)) ++ (push_int rsi)
-        | SUBI (t1, t2) -> (code_of t1) ++ (code_of t2) ++ (get_two) ++ (subq (reg rdi) (reg rsi)) ++ (push_int rsi)
+        | ADDI (t1, t2) -> (code t1 t2) ++ (pop_int rdi) ++ (pop_int rsi) ++ (addq (reg rdi) (reg rsi)) ++ (push_int rsi)
+        | SUBI (t1, t2) -> (code t1 t2) ++ (pop_int rdi) ++ (pop_int rsi) ++ (subq (reg rdi) (reg rsi)) ++ (push_int rsi)
+        | MULI (t1, t2) -> (code t1 t2) ++ (pop_int rdi) ++ (pop_int rsi) ++ (imulq (reg rdi) (reg rsi)) ++ (push_int rsi)
+        | DIVI (t1, t2) -> (code t1 t2) ++ (pop_int rax) ++ (pop_int rdi) ++ (movq (imm 0) (reg rdx)) ++ (idivq (reg rdi)) ++ (push_int rax)
+        | MODI (t1, t2) -> (code t1 t2) ++ (pop_int rax) ++ (pop_int rdi) ++ (movq (imm 0) (reg rdx)) ++ (idivq (reg rdi)) ++ (push_int rdx)
+        | NEGI t -> (code_of t) ++ (pop_int rdi) ++ (negq (reg rdi)) ++ (push_int rdi)
         | _ -> failwith "not implemented in assembly yet";;
 
 let print_int_fun =
@@ -37,3 +41,4 @@ let assembly_of_tree tree =
         X86_64.print_program fmt code;
         close_out c;;
 
+assembly_of_tree (ADDI (INT "3", SUBI (INT "5", MULI (NEGI (INT "2"), DIVI (INT "4", MODI (INT "10", INT "7"))))));;
